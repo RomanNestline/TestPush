2610d7f5313d5845e2785843d906f269bf6e328b
Roman <roman@Roman-Mac.local>
2015-08-19 16:58:35 +0300

    Initial Commit

---
+++ TestPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPush
+//
+//  Created by Roman on 19/08/15.
+//  Copyright (c) 2015 Nestline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestPush/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestPush
+//
+//  Created by Roman on 19/08/15.
+//  Copyright (c) 2015 Nestline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
